b28124f16a97d861b1ba38183d343be4b5fe6c1e
Daniel Dominguez Cabello <ddoming2@c2r7s1.42madrid.com>
2025-10-15 22:47:59 +0200

    me voy a cenar

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ddoming2 <ddoming2@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 20:30:17 by ddoming2          #+#    #+#             */
-/*   Updated: 2025/10/04 19:51:56 by ddoming2         ###   ########.fr       */
+/*   Updated: 2025/10/15 22:33:20 by ddoming2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -20,7 +20,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	i = 0;
 	destination = (char *)dest;
 	source = (const char *)src;
-	while ((i < n) && (source[i] != '\0'))
+	while ((i < n))
 	{
 		destination[i] = source[i];
 		i++;
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ddoming2 <ddoming2@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 19:46:47 by ddoming2          #+#    #+#             */
-/*   Updated: 2025/10/15 21:40:49 by ddoming2         ###   ########.fr       */
+/*   Updated: 2025/10/15 22:46:55 by ddoming2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*destination;
-	char	*temp;
+	unsigned char	*destination;
+	unsigned char	*temp;
 
-	destination = (char *)dest;
-	temp = (char *)src;
+	destination = (unsigned char *)dest;
+	temp = (unsigned char *)src;
 	ft_memcpy((void *)destination, (const void *)temp, n);
 	return (dest);
 }
