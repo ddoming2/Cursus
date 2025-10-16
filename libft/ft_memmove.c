fa93e64104a4f702b716dd4dbd07e9ead245398e
Daniel Dominguez Cabello <ddoming2@c2r1s5.42madrid.com>
2025-10-16 20:30:16 +0200

    memmove fix & strlcpy

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ddoming2 <ddoming2@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 19:46:47 by ddoming2          #+#    #+#             */
-/*   Updated: 2025/10/15 22:46:55 by ddoming2         ###   ########.fr       */
+/*   Updated: 2025/10/16 19:59:01 by ddoming2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,15 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	destination = (unsigned char *)dest;
 	temp = (unsigned char *)src;
-	ft_memcpy((void *)destination, (const void *)temp, n);
+	if (temp < destination)
+	{
+		while (n > 0)
+		{
+			n--;
+			destination[n] = temp[n];
+		}
+	}
+	else
+		ft_memcpy((void *)destination, (const void *)temp, n);
 	return (dest);
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,35 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy_v2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddoming2 <ddoming2@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/04 19:54:44 by ddoming2          #+#    #+#             */
-/*   Updated: 2025/10/15 20:08:34 by ddoming2         ###   ########.fr       */
+/*   Created: 2025/10/16 20:06:37 by ddoming2          #+#    #+#             */
+/*   Updated: 2025/10/16 20:28:23 by ddoming2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	ssize;
-	size_t	dsize;
 
 	i = 0;
-	ssize = sizeof(src);
-	dsize = sizeof(dst);
 	if (size > 0)
 	{
-		while (size - 1 > i && i < ssize && i < dsize)
+		while (src[i] != '\0' && size - 1 > i)
 		{
-			dst[i] = src[i];
+			dst[i] = src [i];
 			i++;
 		}
-		dst[i] = '\0';
 	}
 	else
-		return (0);
-	return (ssize);
+		return (ft_strlen(src));
+	dst[i] = '\0';
+	return (ft_strlen(src));
 }
