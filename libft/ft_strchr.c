2f1eba80bfc998e29ad451bf47e535547dc600e9
Daniel Dominguez Cabello <ddoming2@c2r4s1.42madrid.com>
2025-10-19 19:45:44 +0200

    fixed strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ddoming2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 19:12:22 by ddoming2          #+#    #+#             */
-/*   Updated: 2025/10/17 06:38:33 by ddoming2         ###   ########.fr       */
+/*   Updated: 2025/10/19 19:44:15 by ddoming2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,11 +14,13 @@
 char	*ft_strchr(const char *s, int c)
 {
 	unsigned int	i;
+	unsigned char	character;
 
 	i = 0;
-	while (s[i] != '\0' && s[i] != c)
+	character = (unsigned char)c;
+	while (s[i] != '\0' && s[i] != character)
 		i++;
-	if (s[i] != '\0')
+	if (s[i] == character)
 		return ((char *)&s[i]);
 	return (NULL);
 }
