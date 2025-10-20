c338691c5f6cee4c4529e179a3f7fde7969383cb
ddoming2 <ddoming2@student.42madrid.com>
2025-10-20 07:19:04 +0200

    added memrchr (not needed)

--- libft/ft_memrchr_NOT_NEEDED.c
+++ libft/ft_memrchr_NOT_NEEDED.c
@@ -1,15 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddoming2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 19:12:22 by ddoming2          #+#    #+#             */
-/*   Updated: 2025/10/20 06:54:44 by ddoming2         ###   ########.fr       */
+/*   Updated: 2025/10/20 07:15:55 by ddoming2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
+ * DIO ERROR IMPLICIT DECLARATION MEMRCHR ORIGINAL (NO SE HA PODIDO PROBAR)
  * FALTA MANEJAR ERROR DE N > CONST VOID *S[N]
  * FALTA QUITAR MAIN
  * FALTA PASAR PACO
@@ -18,21 +19,24 @@
 #include <string.h>
 #include <stdio.h>
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void	*ft_memrchr(const void *s, int c, size_t n)
 {
-	size_t		i;
-	unsigned char	character;
-	const char	*source;
+	char	*character;
+	char	*source;
+	unsigned int	i;
 
-
-	i = 0;
-	character = (unsigned char)c;
-	source = (const char *)s;
-	while (source[i] != '\0' && source[i] != character && i < n)
+	character = NULL;
+	source = (char *)s;
+	while (*source && i < n)
+	{
+		if ((unsigned char)*source == (unsigned char)(c))
+			character = source;
+		source++;
 		i++;
-	if (source[i] == character)
-		return ((void *)&source[i]);
-	return (NULL);
+	}
+	if ((unsigned char)c == '\0')
+		return (source);
+	return (character);
 }
 
 int	main(void)
@@ -41,7 +45,7 @@ int	main(void)
 	int 	c = 'o';
 	size_t	n = 5;
 
-	printf("%s\n", (char *)memchr(s, c, n));
-	printf("%s\n", (char *)ft_memchr(s, c, n));
+	printf("%s\n", (char *)memrchr(s, c, n));
+	printf("%s\n", (char *)ft_memrchr(s, c, n));
 	return (0);
 }
